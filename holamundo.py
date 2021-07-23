e397256cf1193ccf82ea983ecd0ede829ecd9acd
Carlos Daniel Marceleño Rodriguez <carlosd.marceleno@gmail.com>
2021-07-23 00:52:53 -0500

    Update holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -7,3 +7,10 @@ print("Hola pandas")
 print("Hola matplotlib")
 print("Hola analista")
 print("Hola sktlearn")
+def functionpy(a):
+    if a==1:
+        b=2
+        return {True,b,5200}
+    else:
+        return False
+print(functionpy(1))
